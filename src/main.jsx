e9ed971a87f3366cc41d4a407e0c30ea6aee3f40
Ayokunle Dalemo <63431725+aykdgreat@users.noreply.github.com>
2025-05-05 23:55:16 +0100

    Updated router to use createRoutesFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import LandingPage  from './components/LandingPage'
-import Login from './components/Login'
-import Signup from './components/signup'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Signup from "./components/signup";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>Error Page</div>,
-    children: [
-      { index: true, element: <Login /> }, 
-      { path: "login", element: <Login /> },
-      { path: "signup", element: <Signup /> },
-      { path: "Landingpage", element: <LandingPage /> }, // Default to login
-    ]
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    // {
+    //   path: "/",
+    //   element: <App />,
+    //   errorElement: <div>Error Page</div>,
+    //   children: [
+    //     { index: true, element: <Login /> },
+    //     { path: "login", element: <Login /> },
+    //     { path: "signup", element: <Signup /> },
+    //     { path: "Landingpage", element: <LandingPage /> }, // Default to login
+    //   ],
+    // },
+    <Route path="/" element={<App />}>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Landingpage" element={<LandingPage />} />
+    </Route>,
+  ])
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
